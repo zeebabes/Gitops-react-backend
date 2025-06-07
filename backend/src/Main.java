689be5390bd4002629f534f41292ec24790e19bd
KemiA <kzagbabiaka@gmail.com>
2025-06-07 14:45:29 -0400

    update

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,7 +1,28 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-package com.cyat.backend;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Backend service for GitOps project running...");
+    public static void main(String[] args) throws IOException {
+        int port = 8090;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // Use default executor
+        server.start();
+        System.out.println("Java backend running on http://localhost:" + port);
     }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "GitOps Java backend is running!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
