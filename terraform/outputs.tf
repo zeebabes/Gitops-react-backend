21dcd2501baf64bb741bc4c5a53a818ef2f0ef67
KemiA <kzagbabiaka@gmail.com>
2025-06-07 13:07:34 -0400

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,15 @@
 output "cluster_name" {
-  description = "EKS Cluster name"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_id
 }
 
 output "cluster_endpoint" {
-  description = "EKS Cluster endpoint"
-  value       = module.eks.cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
-  description = "Security Group associated with EKS Cluster"
-  value       = module.eks.cluster_security_group_id
+  value = module.eks.cluster_security_group_id
 }
 
 output "region" {
-  description = "AWS Region"
-  value       = var.aws_region
+  value = var.aws_region
 }
