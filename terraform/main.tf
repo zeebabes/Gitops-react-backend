114b3eae533e40dd37b63afe0d940c1cada3453c
KemiA <kzagbabiaka@gmail.com>
2025-06-07 13:06:25 -0400

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,44 +4,12 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11"
-    }
   }
-
   required_version = ">= 1.3.0"
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-# Retrieve cluster info for Kubernetes + Helm access
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
 }
 
 # VPC Module
@@ -64,40 +32,27 @@ module "vpc" {
   }
 }
 
-# EKS Module (uses `subnets` instead of `vpc_subnet_ids`)
+# EKS Cluster Module (v17.x, uses `subnets`)
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.24.0"  # ✅ version that supports `subnets`
+  version = "17.24.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
   vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets  # 👈 This works in v17.x
+  subnets         = module.vpc.private_subnets
 
   node_groups = {
-  default = {
-    desired_capacity = 2
-    max_capacity     = 3
-    min_capacity     = 1
-    instance_type    = "t3.medium"
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
   }
-}
 
   tags = {
     Environment = "gitops"
     Terraform   = "true"
   }
-}
-
-# Install ArgoCD with Helm
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  create_namespace = true
-
-  values = [
-    file("${path.module}/argocd-values.yaml")
-  ]
 }
