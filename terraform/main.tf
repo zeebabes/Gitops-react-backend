e4a456be200c1a5288661674e1ef1cd244336dc2
KemiA <kzagbabiaka@gmail.com>
2025-06-07 17:40:56 -0400

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,51 +6,51 @@ data "aws_availability_zones" "available" {}
 
 ################### VPC + NAT Gateway Networking ###################
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "zeebabes-vpc" }
+  tags = { Name = "main-vpc" }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = { Name = "zeebabes-igw" }
+  vpc_id = aws_vpc.vpc.id
+  tags = { Name = "main-igw" }
 }
 
-resource "aws_eip" "nat" {
+resource "aws_eip" "nat_eip" {
   domain = "vpc"
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public[0].id
-  tags = { Name = "zeebabes-nat-gw" }
+  tags = { Name = "main-nat-gw" }
 
   depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = { Name = "zeebabes-public-rt" }
+  tags = { Name = "main-public-rt" }
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
-  tags = { Name = "zeebabes-private-rt" }
+  tags = { Name = "main-private-rt" }
 }
 
 resource "aws_subnet" "public" {
   count                   = 2
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet("10.0.0.0/16", 4, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
@@ -59,7 +59,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count             = 2
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet("10.0.0.0/16", 4, count.index + 2)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = { Name = "private-${count.index}" }
@@ -68,13 +68,13 @@ resource "aws_subnet" "private" {
 resource "aws_route_table_association" "public_assoc" {
   count          = 2
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "private_assoc" {
   count          = 2
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
 ################### IAM Roles ###################
@@ -96,7 +96,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "node_group_role" {
+resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -109,24 +109,24 @@ resource "aws_iam_role" "node_group_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "worker_node_policy" {
-  role       = aws_iam_role.node_group_role.name
+  role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cni_policy" {
-  role       = aws_iam_role.node_group_role.name
+  role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "registry_policy" {
-  role       = aws_iam_role.node_group_role.name
+  role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 ################### EKS Cluster + Nodes ###################
 
-resource "aws_eks_cluster" "zeebabes" {
-  name     = "zeebabes"
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
@@ -136,10 +136,10 @@ resource "aws_eks_cluster" "zeebabes" {
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "aws_eks_node_group" "default" {
-  cluster_name    = aws_eks_cluster.zeebabes.name
-  node_group_name = "zeebabes-node-group"
-  node_role_arn   = aws_iam_role.node_group_role.arn
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
@@ -166,39 +166,39 @@ resource "aws_eks_node_group" "default" {
 
 ################### Kubernetes & Helm Providers ###################
 
-data "aws_eks_cluster" "zeebabes" {
-  name = aws_eks_cluster.zeebabes.name
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
 }
 
-data "aws_eks_cluster_auth" "zeebabes" {
-  name = aws_eks_cluster.zeebabes.name
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.zeebabes.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.zeebabes.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.zeebabes.token
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.zeebabes.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.zeebabes.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.zeebabes.token
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster.token
   }
 }
 
 ################### aws-auth ConfigMap ###################
 
 resource "kubernetes_config_map" "aws_auth" {
-  depends_on = [aws_eks_node_group.default]
+  depends_on = [aws_eks_node_group.node_group]
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
   }
   data = {
     mapRoles = yamlencode([{
-      rolearn  = aws_iam_role.node_group_role.arn
+      rolearn  = aws_iam_role.eks_node_role.arn
       username = "system:node:{{EC2PrivateDNSName}}"
       groups   = [
         "system:bootstrappers",
@@ -221,23 +221,23 @@ resource "helm_release" "argocd" {
   values = [file("argocd-values.yaml")]
 
   depends_on = [
-    aws_eks_cluster.zeebabes,
-    aws_eks_node_group.default
+    aws_eks_cluster.eks_cluster,
+    aws_eks_node_group.node_group
   ]
 }
 
 ################### Outputs ###################
 
 output "cluster_name" {
-  value = aws_eks_cluster.zeebabes.name
+  value = aws_eks_cluster.eks_cluster.name
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.zeebabes.endpoint
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region us-east-2 --name ${aws_eks_cluster.zeebabes.name}"
+  value = "aws eks update-kubeconfig --region us-east-2 --name ${aws_eks_cluster.eks_cluster.name}"
 }
 
 output "dashboard_access_command" {
