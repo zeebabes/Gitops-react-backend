2a39d0e03775d8cfc5e07cd86e41bb7320e58015
KemiA <kzagbabiaka@gmail.com>
2025-06-07 13:14:42 -0400

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,113 @@
-output "cluster_name" {
-  value = module.eks.cluster_id
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+
+  required_version = ">= 1.3.0"
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+provider "aws" {
+  region = var.aws_region
 }
 
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
+# VPC Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.2"
+
+  name = "gitops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "gitops-vpc"
+  }
 }
 
-output "region" {
-  value = var.aws_region
+# EKS Cluster Module
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+
+  # ✅ Prevent circular dependency
+  manage_aws_auth = false
+
+  tags = {
+    Environment = "gitops"
+    Terraform   = "true"
+  }
+}
+
+# Delay EKS access until created
+data "aws_eks_cluster" "cluster" {
+  name       = module.eks.cluster_id
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name       = module.eks.cluster_id
+  depends_on = [module.eks]
+}
+
+# Kubernetes provider (used by Helm)
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+  load_config_file = false
+}
+
+# ArgoCD Helm release
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/argocd-values.yaml")
+  ]
+
+  depends_on = [module.eks]
 }
